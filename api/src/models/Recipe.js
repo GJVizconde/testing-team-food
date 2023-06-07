c8298ff1a8842ab33fafb673df06a8d500536ea9
SH-ur <sdjsm2003@gmail.com>
2023-06-07 16:42:38 -0400

    Santi's DB Trial

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,30 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("recipe", {
+    ID: {type: DataTypes.UUID,
+      primaryKey: true,
+    defaultValue: UUIDV4},
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+dishResume:{
+  type: DataTypes.STRING,
+  allowNull:false
+},
+health_score:{
+  type: DataTypes.INTEGER
+},
+steps:{
+  type: DataTypes.STRING
+}
+  },
+  {timestamps: false});
 };
