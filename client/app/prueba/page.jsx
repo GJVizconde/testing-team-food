75a9015a7b6434ffc3414942be33b4cce964dcf4
GJVizconde <gilmerjose.vizconde2@gmail.com>
2023-06-10 11:55:43 -0500

    ApiUser test

--- client/app/prueba/page.jsx
+++ client/app/prueba/page.jsx
@@ -1,15 +1,25 @@
 'use client';
 
+import { useGetUsersQuery } from '../../redux/services/userApi';
 import { increment, decrement } from '../../redux/features/counterSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 const CounterTest = () => {
 	const count = useAppSelector((state) => state.counterReducer.counter);
 	const dispatch = useAppDispatch();
+
+	const { data, error, isLoading, isFetching } = useGetUsersQuery(null);
+
+	if (isLoading || isFetching) return <p>Loading...</p>;
+	if (error) return <p>Some error</p>;
+
+	useGetUsersQuery;
+
 	return (
 		<div>
 			<h1>total: {count}</h1>
 			<button
+				className='bg-green-500 px-3 py-2'
 				onClick={() => {
 					dispatch(increment());
 				}}
@@ -21,9 +31,18 @@ const CounterTest = () => {
 				onClick={() => {
 					dispatch(decrement());
 				}}
+				className='bg-blue-500 px-3 py-2'
 			>
 				Decrement
 			</button>
+
+			{data?.map((user) => (
+				<div>
+					<p>{user.name}</p>
+					<p>{user.username}</p>
+					<p>{user.email}</p>
+				</div>
+			))}
 		</div>
 	);
 };
