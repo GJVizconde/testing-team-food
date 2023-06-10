6174833ac419cd7875826b52272835e10e5455de
GJVizconde <gilmerjose.vizconde2@gmail.com>
2023-06-09 19:35:46 -0500

    feature:counterSlice

---
+++ client/redux/features/counterSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    counter:0
+}
+
+export const counterSlice =createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        increment:(state)=>{
+            state.counter+=1
+        },
+        decrement:(state)=>{
+            state.counter-=1
+        }
+    }
+})
+
